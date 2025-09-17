2c36b8da3b934debf2f51d6abcf78bba903dccb7
mansuramir <mansuramir@pd.jaring.my>
2025-09-17 14:59:00 +0800

    add polybar battery module script

---
+++ Systems/obsd-x13/.config/polybar/script/battery.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+BAT_INFO=$(acpi -b)
+BAT=$(echo "$BAT_INFO" | grep -E -o '[0-9]+%' | head -1)
+BAT_VALUE=${BAT%?}  
+# Remove the '%' sign
+
+FULL_ICON="   "  
+# Full battery
+MEDIUM_ICON="   "  
+# Medium battery
+QUARTER_ICON="   "  
+# Quarter battery
+EMPTY_ICON="   "  
+# Empty battery
+CHARGING_ICON="󱐋 "  
+# Charging icon
+
+CHARGING_STATUS=$(echo "$BAT_INFO" | grep -o 'Charging')
+
+if [ "$CHARGING_STATUS" = "Charging" ]; then
+    ICON="$CHARGING_ICON"
+    COLOR="#00cccc"
+elif [ "$BAT_VALUE" -gt 80 ]; then
+    ICON="$FULL_ICON"
+    COLOR="#00cccc"  
+# Purple color
+elif [ "$BAT_VALUE" -gt 60 ] && [ "$BAT_VALUE" -le 80 ]; then
+    ICON="$MEDIUM_ICON"
+    COLOR="#00cccc"  
+# Purple color
+elif [ "$BAT_VALUE" -gt 35 ] && [ "$BAT_VALUE" -le 60 ]; then
+    ICON="$QUARTER_ICON"
+    COLOR="#cc0088"  
+# Warning color
+elif [ "$BAT_VALUE" -gt 20 ] && [ "$BAT_VALUE" -le 35 ]; then
+    ICON="$QUARTER_ICON"
+    COLOR="#cc0022"  
+# Red-ish color
+elif [ "$BAT_VALUE" -le 20 ]; then
+    ICON="$EMPTY_ICON"
+    COLOR="#880000"  
+# Red color for empty battery
+    URGENT=33  
+# Urgent flag
+# else
+#    ICON="$QUARTER_ICON"  # Fallback icon
+#    COLOR="#ff00ff"  # Default color
+fi
+
+# Full and short texts
+FULL_TEXT=" $ICON $BAT "
+SHORT_TEXT="BAT: $BAT"
+
+# Output
+echo "$FULL_TEXT "
+echo "$SHORT_TEXT "
+echo "$COLOR"
+[ -n "$URGENT" ] && exit "$URGENT"
+
+exit 0
